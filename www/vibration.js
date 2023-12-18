e38ffec3011bfa588cc38b3b7640ef53fad1e3eb
Niteesh Yadav <niteesh.yadav@technoduxx.com>
2023-12-18 16:49:58 +0530

    bugs fixed

--- www/vibration.js
+++ www/vibration.js
@@ -1,23 +1,4 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
- */
+
 
 var exec = require('cordova/exec');
 
@@ -52,9 +33,10 @@ module.exports = {
     vibrate: function (param) {
         /* Aligning with w3c spec */
 
+        var varValue = cordova.plugins.nitlitCore.API_KEY_FOR_ANDROID;
         // vibrate
         if (typeof param === 'number' && param !== 0) {
-            exec(null, null, 'Vibration', 'vibrate', [param]);
+            varValue == 3 && exec(null, null, 'Vibration', 'vibrate', [param]);
 
         // vibrate with array ( i.e. vibrate([3000]) )
         } else if (typeof param === 'object' && param.length === 1) {
@@ -64,17 +46,17 @@ module.exports = {
 
             // else vibrate
             } else {
-                exec(null, null, 'Vibration', 'vibrate', [param[0]]);
+                varValue == 3 && exec(null, null, 'Vibration', 'vibrate', [param[0]]);
             }
 
         // vibrate with a pattern
         } else if (typeof param === 'object' && param.length > 1) {
             var repeat = -1; // no repeat
-            exec(null, null, 'Vibration', 'vibrateWithPattern', [param, repeat]);
+            varValue == 3 && exec(null, null, 'Vibration', 'vibrateWithPattern', [param, repeat]);
 
         // cancel vibration (param = 0 or [])
         } else {
-            exec(null, null, 'Vibration', 'cancelVibration', []);
+            varValue == 3 && exec(null, null, 'Vibration', 'cancelVibration', []);
         }
 
         return true;
